e30468cce0f17a667c2670ccd48c222203feb02b
Austin Alcancia <aalcanci@oberlin.edu>
2022-11-12 16:15:29 -0500

    Updated Code

--- tree.py
+++ tree.py
@@ -18,7 +18,7 @@ pd.options.mode.chained_assignment = None
 #handling command line arguments to be entered in the terminal
 training_percentage=float(sys.argv[2])
 random_seed= int(sys.argv[3])
-is_numeric=bool(sys.argv[4])
+is_numeric=str(sys.argv[4])
 dataset=sys.argv[1]
 
 random.seed(random_seed)
@@ -47,6 +47,7 @@ class node:
     def __init__(self, val): 
         self.val = val
         self.children = {}
+        self.threshold = None
 
 #calculate entropy of a dataframe
 def calculate_entropy(dataframe):
@@ -103,25 +104,68 @@ def gain(S, a):
     gain = calculate_entropy(S) - second_exp
     return gain
 
-def numeric_gain(S, a):
-    # print("this is gain")
+def threshold(S, a):
+    S = S.sort_values(a)
     # print(S)
-    # print('this is attribute')
-    # print(a)
-    gain_attribute = {}
+    label_index = 0
     attribute_index = columnnames.index(a)
-    length = len(S)
-    S.sort_values(by=[a])
+    thresholds = []
+    previous = row=S.iloc[0].to_numpy()
+    for i in range(1,len(S)):
+        row=S.iloc[i].to_numpy()
+        if previous[label_index] != row[label_index]:
+            # print("previous is: ", previous[label_index], previous[attribute_index])
+            # print("row is: ", row[label_index], row[attribute_index])
+            new_threshold = (previous[attribute_index]+row[attribute_index])/2
+            thresholds.append(new_threshold)
+            previous = row
+    return thresholds
+
+def best_threshold(S, a, thresholds):
+    best_gain = 0 
+    best_thresh = 0
+    for threshold in thresholds:
+        gain = numeric_gain(S, a, threshold)
+        if gain > best_gain:
+            best_gain = gain
+            best_thresh = threshold
+    return best_thresh
+
+def numeric_entropy(S, a, threshold):
+    attribute_index = columnnames.index(a)
+    less = {}
+    greater ={}
+    label_index = 0
+    less_total = 0
+    greater_total = 0
     for i in range(0,len(S)):
         row=S.iloc[i].to_numpy()
-        if row[attribute_index] not in gain_attribute:
-            gain_attribute[row[attribute_index]]=1
+        if row[attribute_index] <= threshold:
+            if row[label_index] not in less:
+                less[row[label_index]]=1
+            else:
+                less[row[label_index]]+=1
+            less_total+=1
         else:
-            gain_attribute[row[attribute_index]]+=1
-    second_exp = 0
-    for attribute_value in gain_attribute.keys():
-        second_exp = second_exp + ((gain_attribute[attribute_value])/length) *  calculate_attribute_entropy(S, attribute_value, attribute_index)
-    gain = calculate_entropy(S) - second_exp
+            if row[label_index] not in greater:
+                greater[row[label_index]]=1
+            else:
+                greater[row[label_index]]+=1
+            greater_total+=1
+    entropy_less = 0
+    entropy_greater = 0
+    for values in less.values():
+        entropy_less = entropy_less + ((values/less_total) * math.log2(values/less_total))
+    entropy_less = -1*entropy_less
+    for values in greater.values():
+        entropy_greater = entropy_greater + ((values/greater_total) * math.log2(values/greater_total))
+    entropy_greater = -1*entropy_greater
+    return entropy_less, entropy_greater, less_total, greater_total
+
+def numeric_gain(S, a, threshold):
+    entropy_less, entropy_greater, less_total, greater_total = numeric_entropy(S, a, threshold)
+    length = len(S)
+    gain = 1 - ((less_total/length) * entropy_less) + ((greater_total/length) * entropy_greater) 
     return gain
 
 def best_attribute(attributes, S):
@@ -152,8 +196,51 @@ def possible_values(attribute, subset, index):
     # print(new_df)
     return new_df
 
+def new_df_numeric(attribute, subset, threshold):
+    less_df = subset[subset[attribute] <= threshold]
+    greater_df = subset[subset[attribute] > threshold]
+    return less_df, greater_df
+
 def ID3(attributes, subset):
-   
+
+    if is_numeric=="True":
+        print("this is true")
+        label_count = {}
+        for i in range(len(subset)):
+            row=subset.iloc[i].to_numpy()
+            if row[0] not in label_count:
+                label_count[row[0]]=1
+            else:
+                label_count[row[0]]+=1
+        max_label = max(label_count, key=label_count.get)
+        if len(attributes)==0:
+            # print("length is 0")
+            N = node(max_label)
+        elif len(label_count)==1:
+            N = node(max_label)
+        else:
+            best = best_attribute(attributes, subset)
+            list_thresholds = threshold(subset, best)
+            threshold_best = best_threshold(subset, best, list_thresholds)
+            attribute_index = columnnames.index(best)
+            N = node(best)
+            N.threshold = threshold_best
+            # unique = attribute_uniquevalues[best]
+            pass_attribute=copy.deepcopy(attributes)
+            pass_attribute.remove(best)
+            less_df, greater_df = new_df_numeric(best, subset, threshold)
+            if len(less_df)==0:
+                N.children['less'] = node(max_label)
+            else:
+                N.children['less']=ID3(pass_attribute, less_df)
+            if len(greater_df)==0:
+                N.children['greater'] = node(max_label)
+            else:
+                N.children['greater']=ID3(pass_attribute, greater_df)
+        return N
+
+    if is_numeric=="False":
+        print('This is false')
         label_count = {}
         for i in range(len(subset)):
             row=subset.iloc[i].to_numpy()
@@ -212,9 +299,11 @@ def printTree(tree:node, level=0,child=""):
         printTree(tree.children[child], level + 1,child)
 
 
-tree = ID3(attributes, training_df)
-prediction(test_df,tree,columnnames)
+# tree = ID3(attributes, training_df)
+# prediction(test_df,tree,columnnames)
 
+thresholds = threshold(training_df, "bill_length_mm")
+print(thresholds)
 
 
 
